7d6d813e1eddb06a4513784c6b325f64f7b79713
Orlando Watson <ogw@orlandogwatson.com>
2016-09-02 19:48:05 -0400

    dashboard up

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var passport = require('passport');
 var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
+var axios = require('axios');
 var router = express.Router();
+
+var env = {
+  API_URL: process.env.API_URL || 'http://localhost:4242',
+  API_USER_NAME: process.env.API_USER_NAME || 'admin',
+  API_USER_PASSWORD: process.env.API_USER_PASSWORD || 'password'
+};
 
 /* GET user profile. */
 router.get('/', ensureLoggedIn, function(req, res, next) {
-  res.render('dashboard', { user: req.user });
+  axios.get(env.API_URL + '/api/images')
+    .then(function(response) {
+      res.render('dashboard', { title: 'Dashboard', user: req.user, images: response.data });
+    })
 });
 
 module.exports = router;
