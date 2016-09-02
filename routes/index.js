6563b788a577f3006bb741089cd9433f961b68ea
Orlando Watson <ogw@orlandogwatson.com>
2016-09-02 16:27:09 -0400

    Make Login Work

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,26 @@
 var express = require('express');
 var passport = require('passport');
+var axios = require('axios');
 var router = express.Router();
 
 var env = {
   AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
   AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-  AUTH0_CALLBACK_URL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+  AUTH0_CALLBACK_URL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback',
+  API_URL: process.env.API_URL || 'http://localhost:4242',
+  API_USER_NAME: process.env.API_USER_NAME || 'admin',
+  API_USER_PASSWORD: process.env.API_USER_PASSWORD || 'password'
 };
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', env: env });
-});
+router.get('/', function(req, res){
+    res.render('index', { env: env });
+  }
+);
 
-router.get('/login',
-  function(req, res){
-    res.render('login', { env: env });
-  });
+router.get('/login', function(req, res) {
+  res.redirect('/');
+});
 
 router.get('/logout', function(req, res){
   req.logout();
@@ -26,7 +30,18 @@ router.get('/logout', function(req, res){
 router.get('/callback',
   passport.authenticate('auth0', { failureRedirect: '/url-if-something-fails' }),
   function(req, res) {
-    res.redirect(req.session.returnTo || '/user');
+    //authenticate user over api to get token
+    axios.post(env.API_URL + '/authenticate', {
+      username: env.API_USER_NAME,
+      password: env.API_USER_PASSWORD
+    }).then(function(response) {
+      if(response.data.error) {
+        res.redirect('/logout');
+      }
+
+      res.cookie('auth', response.data.token);
+      res.redirect(req.session.returnTo || '/dashboard');
+    });
   });
 
 
